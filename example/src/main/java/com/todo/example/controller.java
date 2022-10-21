0684fc604398c94a04de37080f3669411915c583
MinjounKim <rlaalswns804@gmail.com>
2022-10-21 16:49:51 +0900

    add controller

---
+++ example/src/main/java/com/todo/example/controller.java
@@ -0,0 +1,14 @@
+package com.todo.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @GetMapping("/")
+    public String hello(){
+        return "To-do Application !";
+        
+    }
+}
